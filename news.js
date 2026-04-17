ac6da8a4f9f0ad3e4272e3eda0d2c980c455da67
Mash_xf <felix.macharia@student.moringaschool.com>
2026-04-17 10:33:41 +0300

    js variables

---
+++ news.js
@@ -0,0 +1,9 @@
+//variable
+const API_KEY = "f14f836054c9449fac35521410817c4b";
+const button = document.getElementById("fetch-news");
+const display = document.getElementById("news-container");
+const searchInput = document.getElementById("search");
+const categorySelect = document.getElementById("category-select");
+const categoryList = document.getElementById("category-list");
+const statusText = document.getElementById("news-status");
+
